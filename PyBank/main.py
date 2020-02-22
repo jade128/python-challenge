1467ecc7f1af11797f47b6fa09a8b1ed2d023472
Jade Tao <jadetao@Jades-MacBook-Pro.local>
2020-02-21 18:26:55 -0600

    some changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -30,16 +30,27 @@ with open(Pybank_csv_path) as csvfile:
         TotalMonth +=1
         matrList.append(row)
         profit_last=int(row[1])
-        
-Dicta=dict(matrList)
 
+#set up a new numpy array from list to calculate change of profits
+
+list_arr=np.array(matrList)
+new_np=np.append(list_arr,list_arr,axis=1)
+
+for i in range(85):
+    new_np[i+1][3]=int(new_np[i+1][1])-int(new_np[i][1])
+new_np[0][3]=0
+new_np1=np.delete(new_np,np.s_[1:3],1)
+
+ndict=dict(new_np1)
+#print(ndict)
+
+Dicta=dict(matrList)
 #calculating average for profit change              
 Avg=(profit_last -int(matrList[0][1]))/(TotalMonth-1)
 
 
-
 #convert datatype of values to int than sort dictionary by it values 
-d=dict((k,int(v)) for k,v in Dicta.items())
+d=dict((k,int(v)) for k,v in ndict.items())
 sortedList=sorted(d.items(),key=lambda x:x[1])
 
 
